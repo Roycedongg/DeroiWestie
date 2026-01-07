6e53a97b0267b4cfefc9abda01fa2e0159c20e55
roycedong <502966909@qq.com>
2026-01-06 20:33:22 -0800

    booking链接更改

--- lib/booking.ts
+++ lib/booking.ts
@@ -1,3 +1,4 @@
 export const BOOKING_URL =
 //  "https://calendar.google.com/calendar/u/0/r/appointment?pli=1";
-"https://calendar.app.google/qyTzVBzuf3TUBibq6";
+//"https://calendar.app.google/qyTzVBzuf3TUBibq6";
+"https://calendar.app.google/aN81ZrmsxYbRg1Ad9";
